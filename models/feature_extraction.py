29b95b55c84d590088bb06104cacfd49aed51d27
root <jgyw0910@kaist.ac.kr>
2022-04-29 14:56:59 +0000

    DenseEdgeConv

--- models/feature_extraction.py
+++ models/feature_extraction.py
@@ -1,11 +1,17 @@
 import importlib
 
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
-from torch.nn import Module, Linear, ModuleList
 
-
-
+class DenseEdgeConv(nn.Module):
+    def __init__(self, n_input, n_output):
+        super().__init__()
+        self.n_input = n_input
+        self.n_output = n_output
+        
+    def forward(self, x):
+        pass
 
 
 if __name__ == "__main__":
