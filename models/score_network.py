6da65c7c717411d8474ea3654b680583eef21757
Soulmates2 <kaist984@kaist.ac.kr>
2022-06-02 20:38:33 +0900

    debug scorenet

--- models/score_network.py
+++ models/score_network.py
@@ -1,79 +1,105 @@
-import importlib
-
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 
-class ResBlockConv1d(nn.Module):
-    def __init__(self, c_dim=63, input_size=128, hidden_size=128, output_size=128):
+class ResnetBlockConv1d(nn.Module):
+    """ 1D-Convolutional ResNet block class.
+    Args:
+        size_in (int): input dimension
+        size_out (int): output dimension
+        size_h (int): hidden dimension
+    """
+
+    def __init__(self, c_dim, size_in, size_h=None, size_out=None,
+                 norm_method='batch_norm', legacy=False):
         super().__init__()
-        self.c_dim = c_dim
-        self.input_size = input_size
-        self.hidden_size = hidden_size
-        self.output_size = output_size
-        
-        self.bn1 = nn.BatchNorm1d(self.input_size)
-        self.act1 = nn.ReLU()
-        self.conv1 = nn.Conv1d(self.input_size, self.hidden_size, 1)
-        
-        self.bn2 = nn.BatchNorm1d(self.hidden_size)
-        self.act2 = nn.ReLU()
-        self.conv2 = nn.Conv1d(self.hidden_size, self.output_size, 1)
-        nn.init.zeros_(self.conv2.weight)
+        # Attributes
+        if size_h is None:
+            size_h = size_in
+        if size_out is None:
+            size_out = size_in
 
-        self.conv3 = nn.Conv1d(self.c_dim, self.output_size, 1)
-        
-        self.conv4 = None if input_size == output_size else nn.Conv1d(input_size, output_size, 1, bias=False)
+        self.size_in = size_in
+        self.size_h = size_h
+        self.size_out = size_out
+        # Submodules
+        if norm_method == 'batch_norm':
+            norm = nn.BatchNorm1d
+        elif norm_method == 'sync_batch_norm':
+            norm = nn.SyncBatchNorm
+        else:
+             raise Exception("Invalid norm method: %s" % norm_method)
 
-    
-    def forward(self, x, condition):
-        out = self.conv1(self.act1(self.bn1(x)))
-        out1 = self.conv2(self.act2(self.bn2(out)))
-        out2 = self.conv3(condition)
-        shortcut = x if self.conv4 is None else self.conv4(x)
+        self.bn_0 = norm(size_in)
+        self.bn_1 = norm(size_h)
 
-        return out1 + out2 + shortcut 
+        self.fc_0 = nn.Conv1d(size_in, size_h, 1)
+        self.fc_1 = nn.Conv1d(size_h, size_out, 1)
+        self.fc_c = nn.Conv1d(c_dim, size_out, 1)
+        self.actvn = nn.ReLU()
 
+        if size_in == size_out:
+            self.shortcut = None
+        else:
+            self.shortcut = nn.Conv1d(size_in, size_out, 1, bias=False)
 
+        # Initialization
+        nn.init.zeros_(self.fc_1.weight)
 
-class ScoreNetwork(nn.Module):
-    def __init__(self, point_dim=3, z_dim=60, grad_dim=3, hidden_dim=128, num_blocks=4):
-        super().__init__()
-        self.point_dim = point_dim
-        self.z_dim = z_dim
-        self.grad_dim = grad_dim
-        self.hidden_dim = hidden_dim
-        self.num_blocks = num_blocks
+    def forward(self, x, c):
+        net = self.fc_0(self.actvn(self.bn_0(x)))
+        dx = self.fc_1(self.actvn(self.bn_1(net)))
 
-        self.conv1 = nn.Conv1d(point_dim + z_dim, hidden_dim, 1)
+        if self.shortcut is not None:
+            x_s = self.shortcut(x)
+        else:
+            x_s = x
 
-        self.layer_list = nn.ModuleList()
-        for _ in range(num_blocks):
-            self.layer_list.append(ResBlockConv1d(point_dim + z_dim, hidden_dim))
-        self.bn1 = nn.BatchNorm1d(hidden_dim)
-        self.act1 = nn.ReLU()
-        self.conv2 = nn.Conv1d(hidden_dim, grad_dim, 1)
-
-        # self.layers = nn.Sequential(*self.layer_list, self.bn1, self.act1, self.conv2)      
-    
-    def forward(self, x, z):
-        B, N, D = x.size()
-        point = x.transpose(2, 1)
-        
-        z_expand = z.unsqueeze(2).expand(-1, -1, N)
-        condition = torch.cat([point, z_expand], dim=1)
-
-        out = self.conv1(condition)
-        for layer in self.layer_list:
-            out = layer(out, condition)
-        out = self.conv2(self.act1(self.bn1(out)))
-        # out = self.layers(out, condition)
-        out = out.transpose(2, 1)
+        out = x_s + dx + self.fc_c(c)
 
         return out
 
 
-if __name__ == "__main__":
-    print("============ Score Network Module ============")
-    
+class ScoreNetwork(nn.Module):
+
+    def __init__(self, z_dim=60, dim=3, out_dim=3, hidden_size=128, num_blocks=4):
+        """
+        Args:
+            z_dim:   Dimension of context vectors. 
+            dim:     Point dimension.
+            out_dim: Gradient dim.
+            hidden_size:   Hidden states dim.
+        """
+        super().__init__()
+        self.z_dim = z_dim
+        self.dim = dim
+        self.out_dim = out_dim
+        self.hidden_size = hidden_size
+        self.num_blocks = num_blocks
+
+        # Input = Conditional = zdim (code) + dim (xyz)
+        c_dim = z_dim + dim
+        self.conv_p = nn.Conv1d(c_dim, hidden_size, 1)
+        self.blocks = nn.ModuleList([
+            ResnetBlockConv1d(c_dim, hidden_size) for _ in range(num_blocks)
+        ])
+        self.bn_out = nn.BatchNorm1d(hidden_size)
+        self.conv_out = nn.Conv1d(hidden_size, out_dim, 1)
+        self.actvn_out = nn.ReLU()
+
+    def forward(self, x, c):
+        """
+        :param x: (bs, npoints, self.dim) Input coordinate (xyz)
+        :param c: (bs, self.zdim) Shape latent code
+        :return: (bs, npoints, self.dim) Gradient (self.dim dimension)
+        """
+        p = x.transpose(1, 2)  # (bs, dim, n_points)
+        batch_size, D, num_points = p.size()
+
+        c_expand = c.unsqueeze(2).expand(-1, -1, num_points)
+        c_xyz = torch.cat([p, c_expand], dim=1)
+        net = self.conv_p(c_xyz)
+        for block in self.blocks:
+            net = block(net, c_xyz)
+        out = self.conv_out(self.actvn_out(self.bn_out(net))).transpose(1, 2)
+        return out
